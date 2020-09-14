429f30bcd9c30b8ff8df027110d3a6b878ddf63e
zas <silvan.zahno@hevs.ch>
2020-09-14 11:51:23 +0200

    DEL: old masterVersions

--- COR/hdl/cordicShifter_RTL.vhd
+++
@@ -1,8 +0,0 @@
-ARCHITECTURE masterVersion OF cordicShifter IS
-
-BEGIN
-
-  xShifted <= shift_right(xIn, shiftBitNb);
-  yShifted <= shift_right(yIn, shiftBitNb);
-
-END ARCHITECTURE masterVersion;
